858e6fdd00feb4b83ab05a5a0e8f98072df4e1b8
John Wolfe <wolfejw86@gmail.com>
2020-08-24 02:41:30 -0400

    fixed tailwind production optimization and github deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
+
 module.exports = {
   purge: {
-    enabled: false
+    enabled: true,
+    content: ['./client/**/*.html',
+      './imports/**/*.jsx',
+      './client/**/*.jsx',
+    ],
+    options: {
+      whitelist: [
+        'bg-yellow-200',
+        'bg-red-200',
+        'bg-green-200',
+        'bg-purple-200',
+        'bg-blue-200',
+        'bg-orange-300'
+      ],
+    }
   },
   theme: {},
   variants: {},
